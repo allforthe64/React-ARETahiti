244138dd05a3d71ea4a364552d6a408f30b0bf89
William Rainey <williamhrainey@gmail.com>
2023-04-20 10:43:34 -0700

    added custom or prebuilt selector

--- ARE/src/components/Order.jsx
+++ ARE/src/components/Order.jsx
@@ -4,6 +4,7 @@ const Order = () => {
 
     const [color, setColor] = useState()
     const [openPanel, setOpenPanel] = useState(false)
+    const [custom, setCustom] = useState()
 
     const click = (index) => {
         setColor(index)
@@ -11,21 +12,35 @@ const Order = () => {
         if (openPanel === false) setOpenPanel(true)
     }
 
+    const customClick = (category) => {
+        if (category === 'cust') {
+            setCustom(true)
+        } else {
+            setCustom(false)
+        }
+    }
+
     const content = [
         <div className="border-2 w-9/12 bg-neutral-100 px-3 pt-8 h-[600px] rounded-xl">
             <h1 className="grey heading text-4xl pb-10">Marara V1</h1>
             <div className="flex justify-around">
                 <img src="./marara_6.JPG" className="w-6/12"/>
-                <div className="border w-5/12 flex flex-col items-left">
-                    <p className="orange heading text-2xl text-left mb-2">Select your region</p>
-                    <select className="text-2xl bg-neutral-100 border w-6/12 grey">
-                        <option>United States</option>
-                        <option>Hawaii</option>
-                        <option>Canada</option>
-                        <option>Tahiti</option>
-                        <option>Australia</option>
-                        <option>Pacific Islands</option>
-                    </select>
+                <div className="border w-5/12">
+                    <div className="flex flex-col items-left mb-14">
+                        <p className="orange heading text-2xl text-left mb-2">Select your region</p>
+                        <select className="text-2xl bg-neutral-100 border w-6/12 grey">
+                            <option>United States</option>
+                            <option>Hawaii</option>
+                            <option>Canada</option>
+                            <option>Tahiti</option>
+                            <option>Australia</option>
+                            <option>Pacific Islands</option>
+                        </select>
+                    </div>
+                    <div className="flex justify-around">
+                        <p className={custom ? "orange heading text-2xl text-left mb-2 underline hover:cursor-pointer" : "grey heading text-2xl text-left mb-2 custom-select"} onClick={() => customClick('cust')}>Custom</p>
+                        <p className={custom === false ? "orange heading text-2xl text-left mb-2 underline hover:cursor-pointer" : "grey heading text-2xl text-left mb-2 custom-select"} onClick={() => customClick('preb')}>Prebuilt</p>
+                    </div>
                 </div>
             </div>
         </div>,
