8558eff3b892d99d9630f516e8ef0da2869f6ca6
William Rainey <williamhrainey@gmail.com>
2023-04-24 14:48:24 -0700

    added model selector to form

--- ARE/src/components/Order.jsx
+++ ARE/src/components/Order.jsx
@@ -6,6 +6,7 @@ const Order = () => {
     const [openPanel, setOpenPanel] = useState(false)
     const [custom, setCustom] = useState(true)
     const [custColor, setCustColor] = useState()
+    const [model, setModel] = useState()
     const [c1, setC1] = useState()
     const [c2, setC2] = useState()
     const [c3, setC3] = useState()
@@ -22,8 +23,8 @@ const Order = () => {
 
     const click = (index) => {
         setCanoe(index)
-        console.log('fired')
         setCustColor(null)
+        setModel(null)
         setC1([])
         setC2([])
         setC3([])
@@ -45,14 +46,22 @@ const Order = () => {
 
         setCustColor(e.target.value)
         const colors = e.target.value.split('/')
-
-        console.log(colorsObj[`${colors[1]}`])
         
         setC1([colors[0], colorsObj[colors[0]]])
         setC2([colors[1], colorsObj[colors[1]]])
         setC3([colors[2], colorsObj[colors[2]]])
           
     }
+
+    const modelSelect = (e) => {
+
+        if (e.target.value === 'Select a model') return
+
+        setModel(e.target.value)
+
+    }
+
+    console.log(model)
 
     return (
         <div className="py-20">
@@ -68,10 +77,10 @@ const Order = () => {
                 </div>
             </div>
             <div className={openPanel ? "block flex justify-center" : "hidden"}>
-                <div className="border-2 w-9/12 bg-neutral-100 px-3 pt-8 h-[600px] rounded-xl">
+                <div className="border-2 w-9/12 bg-neutral-100 px-3 pt-8 h-[675px] rounded-xl">
                     <h1 className="grey heading text-4xl pb-10">{canoe === 0 ? 'Marara V1' : canoe === 1 ? 'Mana OC1' : 'Matahina V6'}</h1>
-                    <div className="flex justify-around mb-10">
-                        <img src={canoe === 0 ? "./marara_6.JPG" : canoe === 1 ? "mana_10.JPG" : 'matahina_5.jpg'} className="w-6/12"/>
+                    <div className="flex justify-around ">
+                        <img src={canoe === 0 ? "./marara_6.JPG" : canoe === 1 ? "mana_10.JPG" : 'matahina_5.jpg'} className="w-6/12 h-[50%] mt-10"/>
                         <div className="w-5/12">
                             <div className="flex flex-col items-left mb-14">
                                 <p className="orange heading text-2xl text-left mb-2">Select your region</p>
@@ -90,34 +99,52 @@ const Order = () => {
                                 <p className={custom === false ? "orange heading text-2xl text-left mb-2 underline hover:cursor-pointer" : "grey heading text-2xl text-left mb-2 custom-select"} onClick={() => customClick('preb')}>Prebuilt</p>
                             </div>
                             {custom === true &&
-                                <div className="flex flex-col items-left mb-14">
-                                    <p className="orange heading text-2xl text-left mb-2">Select a color scheme</p>
-                                    <select className="text-2xl bg-neutral-100 border w-10/12 grey" onChange={colorSelect} value={custColor ? custColor : 0}>
-                                        <option>Select a color scheme</option>
-                                        <option>White/White/Yellow</option>
-                                        <option>Blue/Blue/Red</option>
-                                        <option>Dark Blue/Dark Blue/Dark Blue</option>
-                                        <option>White/White/Green</option>
-                                        <option>Light Grey/Light Grey/Blue</option>
-                                        <option>White/White/Blue</option>
-                                        <option>Yellow/Yellow/Yellow</option>
-                                        <option>White/White/Red</option>
-                                        <option>Light Grey/Light Grey/Light Grey</option>
-                                </select>
-                            </div>
+                                <>
+                                    <div className="flex flex-col items-left mb-14">
+                                        <p className="orange heading text-2xl text-left mb-2">Select a model</p>
+                                        {canoe === 0 || canoe === 1 ? 
+                                            <select className="text-2xl bg-neutral-100 border w-10/12 grey" onChange={modelSelect} value={model ? model : 0}>
+                                                <option>Select a model</option>
+                                                <option>Pro Carbon</option>
+                                                <option>Hybrid Carbon</option>
+                                            </select>
+                                        : 
+                                            <select className="text-2xl bg-neutral-100 border w-10/12 grey" onChange={modelSelect} value={model ? model : 0}>
+                                                <option>Select a model</option>
+                                                <option>Wood Iakos/Fiber Glass Ama</option>
+                                                <option>Carbon Iakos/Carbon Ama</option>
+                                            </select>
+                                        }
+                                    </div>
+                                    <div className="flex flex-col items-left mb-14">
+                                        <p className="orange heading text-2xl text-left mb-2">Select a color scheme</p>
+                                        <select className="text-2xl bg-neutral-100 border w-10/12 grey" onChange={colorSelect} value={custColor ? custColor : 0}>
+                                            <option>Select a color scheme</option>
+                                            <option>White/White/Yellow</option>
+                                            <option>Blue/Blue/Red</option>
+                                            <option>Dark Blue/Dark Blue/Dark Blue</option>
+                                            <option>White/White/Green</option>
+                                            <option>Light Grey/Light Grey/Blue</option>
+                                            <option>White/White/Blue</option>
+                                            <option>Yellow/Yellow/Yellow</option>
+                                            <option>White/White/Red</option>
+                                            <option>Light Grey/Light Grey/Light Grey</option>
+                                        </select>
+                                    </div>
+                                </>
                             }
                         </div>
-                        </div>
-                        {(c1 && c2 && c3 && c1.length > 0 && c2.length > 0 && c3.length > 0 && custom === true) && 
-                            <div className="flex justify-around text text-xl grey">
-                                <p className="heading">Hull Color: {c1[0]}</p>
-                                <div className={(c1 && c1.length > 0) ? 'w-[30px] h-[30px] border-black border' : 'hidden'} style={{backgroundColor: c1[1]}}></div>
-                                <p className="heading">Ama Color: {c2[0]}</p>
-                                <div className={(c2 && c2.length > 0) ? 'w-[30px] h-[30px] border-black border' : 'hidden'} style={{backgroundColor: c2[1]}}></div>
-                                <p className="heading">Cockpit Color: {c3[0]}</p>
-                                <div className={(c3 && c3.length > 0) ? 'w-[30px] h-[30px] border-black border' : 'hidden'} style={{backgroundColor: c3[1]}}></div>
-                            </div>    
-                        }
+                    </div>
+                    {(c1 && c2 && c3 && c1.length > 0 && c2.length > 0 && c3.length > 0 && custom === true) && 
+                        <div className="flex justify-around text text-xl grey">
+                            <p className="heading">Hull Color: {c1[0]}</p>
+                            <div className={(c1 && c1.length > 0) ? 'w-[30px] h-[30px] border-black border' : 'hidden'} style={{backgroundColor: c1[1]}}></div>
+                            <p className="heading">Ama Color: {c2[0]}</p>
+                            <div className={(c2 && c2.length > 0) ? 'w-[30px] h-[30px] border-black border' : 'hidden'} style={{backgroundColor: c2[1]}}></div>
+                            <p className="heading">Cockpit Color: {c3[0]}</p>
+                            <div className={(c3 && c3.length > 0) ? 'w-[30px] h-[30px] border-black border' : 'hidden'} style={{backgroundColor: c3[1]}}></div>
+                        </div>    
+                    }
                 </div>
             </div>
         </div>
