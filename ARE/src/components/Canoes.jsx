ba6bc9814065ff2394073706e1de09288a71119a
William Rainey <williamhrainey@gmail.com>
2023-07-05 11:59:24 -0700

    styled canoes page

--- ARE/src/components/Canoes.jsx
+++ ARE/src/components/Canoes.jsx
@@ -10,81 +10,81 @@ const Canoes = () => {
 
     return (
         <div id="en">
-            <div className="flex flex-col items-center py-20">
-                <h1 className="orange text-8xl heading mb-20">Canoes</h1>
-                <p className="text text-xl w-9/12">Ready to take the next step on your outrigger journey?
+            <div className="flex flex-col items-center py-20 max-lg:py-10">
+                <h1 className="orange heading sm:max-md:mb-8 max-sm:mb-10 mb-20 max-sm:text-4xl sm:max-md:text-6xl text-8xl">Canoes</h1>
+                <p className="text text-lg w-9/12 max-sm:w-11/12">Ready to take the next step on your outrigger journey?
                     Well, you're in the right place! Athletes from around the world have 
                     won races in ARE canoes. Now, it's your turn. Check out our three models
                     below.
                 </p>
             </div>
-            <motion.div className="flex items-center flex-col py-20" initial={{ opacity: 0, x:-200 }} whileInView={{ opacity: 1, x:0 }} transition={{ duration: 1, delay: .25}} viewport={{ amount: .5, once: true}}>
-                <h2 className="heading text-6xl grey w-8/12">V6 Matahina - <span className="orange">King Of The Waves</span> </h2>
-                <div className="flex py-10 justify-around">
-                    <img src="./matahina_2.jpg" alt="Matahina Canoe" className="w-5/12" />
-                    <div className="w-4/12 flex items-center">
-                        <div>
-                            <p className="text text-2xl text-left mb-10" id="canoe-text-1">Sleek and sharp looking, ARE Tahiti's 
+            <motion.div className="flex items-center flex-col py-20 max-lg:py-10" initial={{ opacity: 0, x:-50 }} whileInView={{ opacity: 1, x:0 }} transition={{ duration: 1, delay: .25}} viewport={{ amount: .5, once: true}}>
+                <h2 className="heading max-sm:text-2xl sm:max-lg:text-4xl text-6xl grey w-8/12">V6 Matahina - <span className="orange">King Of The Waves</span> </h2>
+                <div className="flex py-10 justify-around max-lg:flex-col max-lg:items-center max-lg:py-4">
+                    <img src="./matahina_2.jpg" alt="Matahina Canoe" className="w-5/12 max-lg:w-8/12 max-lg:mb-6" />
+                    <div className="w-4/12 max-lg:w-[90%] flex items-center">
+                        <div className="max-md:flex max-md:flex-col max-md:items-center">
+                            <p className="text text-2xl max-sm:text-base sm:max-md:text-lg max-lg:text-center text-left max-lg:mb-8 max-md:mb-4 mb-10" id="canoe-text-1">Sleek and sharp looking, ARE Tahiti's 
                                 V6 Matahina is a superb competetive outrigger. Built for the surf,
                                 the Matahina is the perfect boat to launch your racing tradition from.
                             </p>
-                            <Link to={'/matahina'} className="orange orange-border rounded-3xl headings text-3xl px-16 py-2 orange-button transition-color duration-200 ease-in-out">See More</Link>
+                            <Link to={'/matahina'} className="orange orange-border rounded-3xl headings max-md:text-lg max-md:px-10 text-3xl px-16 py-2 orange-button transition-color duration-200 ease-in-out">See More</Link>
                         </div>
                     </div>
                 </div>
             </motion.div>
-            <motion.div className="flex items-center flex-col py-20" initial={{ opacity: 0, x:200 }} whileInView={{ opacity: 1, x:0 }} transition={{ duration: 1, delay: .25}} viewport={{ amount: .5, once: true}}>
-                <h2 className="heading text-6xl grey w-8/12">V1 Marara - <span className="orange">Be At 1</span> </h2>
-                <div className="flex py-10 justify-around">
-                    <div className="w-4/12 flex items-center">
-                        <div>
-                            <p className="text text-2xl text-left mb-10 ml-14">At the forefront of premium 
+            <motion.div className="flex items-center flex-col py-20 max-lg:py-10" initial={{ opacity: 0, x:50 }} whileInView={{ opacity: 1, x:0 }} transition={{ duration: 1, delay: .25}} viewport={{ amount: .5, once: true}}>
+                <h2 className="heading text-6xl grey w-8/12 max-sm:text-2xl sm:max-lg:text-4xl">V1 Marara - <span className="orange">Be At 1</span> </h2>
+                <div className="flex py-10 justify-around max-lg:flex-col-reverse max-lg:items-center max-lg:py-4">
+                    <div className="w-4/12 flex items-center max-lg:w-[90%]">
+                        <div className="max-md:flex max-md:flex-col max-md:items-center">
+                            <p className="text text-2xl text-left max-lg:mb-8 max-md:mb-4 mb-10 lg:ml-14 max-sm:text-base sm:max-md:text-lg max-lg:text-center">At the forefront of premium 
                                 outriggers, the Marara V1 is the ultimate solo outrigger
                                 experience. Are you ready to be "at one"?
                             </p>
-                            <Link to={'/marara'} className="orange orange-border rounded-3xl headings text-3xl px-16 py-2 orange-button transition-color duration-200 ease-in-out">See More</Link>
+                            <Link to={'/marara'} className="orange orange-border rounded-3xl headings max-md:text-lg max-md:px-10 text-3xl px-16 py-2 orange-button transition-color duration-200 ease-in-out">See More</Link>
                         </div>
                     </div>
-                    <img src="./marara_1.JPG" alt="Marara Canoe" className="w-5/12" />
+                    <img src="./marara_1.JPG" alt="Marara Canoe" className="w-5/12 max-lg:w-8/12 max-lg:mb-6" />
                 </div>
             </motion.div>
-            <motion.div className="flex items-center flex-col py-20" initial={{ opacity: 0, x:-200 }} whileInView={{ opacity: 1, x:0 }} transition={{ duration: 1, delay: .25}} viewport={{ amount: .5, once: true}}>
-                <h2 className="heading text-6xl grey w-8/12">V1 Mako - <span className="orange">More of the best</span> </h2>
-                <div className="flex py-10 justify-around">
-                    <img src="./mako_2.jpg" alt="Matahina Canoe" className="w-5/12" />
-                    <div className="w-4/12 flex items-center">
-                        <div>
-                            <p className="text text-2xl text-left mb-10" id="canoe-text-1">Designed for paddlers 180 pounds plus,
+            <motion.div className="flex items-center flex-col py-20 max-lg:py-10" initial={{ opacity: 0, x:-50 }} whileInView={{ opacity: 1, x:0 }} transition={{ duration: 1, delay: .25}} viewport={{ amount: .5, once: true}}>
+                <h2 className="heading text-6xl grey w-8/12 max-sm:text-2xl sm:max-lg:text-4xl">V1 Mako - <span className="orange">More of the best</span> </h2>
+                <div className="flex py-10 justify-around max-lg:flex-col max-lg:items-center max-lg:py-4">
+                    <img src="./mako_2.jpg" alt="Matahina Canoe" className="w-5/12 w-5/12 max-lg:w-8/12 max-lg:mb-6" />
+                    <div className="w-4/12 flex items-center max-lg:w-[90%]">
+                        <div className="max-md:flex max-md:flex-col max-md:items-center">
+                            <p className="text text-2xl text-left max-lg:mb-8 max-md:mb-4 mb-10 lg:ml-14 max-sm:text-base sm:max-md:text-lg max-lg:text-center" id="canoe-text-1">Designed for paddlers 180 pounds plus,
                             the Mako V1 has the same features you love about the Marara, but in a larger, more ergonomic package.
                             </p>
-                            <Link to={'/mako'} className="orange orange-border rounded-3xl headings text-3xl px-16 py-2 orange-button transition-color duration-200 ease-in-out">See More</Link>
+                            <Link to={'/mako'} className="max-md:text-lg max-md:px-10 orange orange-border rounded-3xl headings text-3xl px-16 py-2 orange-button transition-color duration-200 ease-in-out">See More</Link>
                         </div>
                     </div>
                 </div>
             </motion.div>
-            <motion.div className="flex items-center flex-col py-20" initial={{ opacity: 0, x:200 }} whileInView={{ opacity: 1, x:0 }} transition={{ duration: 1, delay: .25}} viewport={{ amount: .4, once: true}}>
-                <h2 className="heading text-6xl grey w-8/12">OC1 Mana - <span className="orange">Raising The Standard</span> </h2>
-                <div className="flex py-10 justify-around">
-                    <img src="./mana_1.JPG" alt="Mana OC1" className="w-5/12" />
-                    <div className="w-4/12 flex items-center">
-                        <div>
-                            <p className="text text-2xl text-left mb-10" id="canoe-text-3">For years, ARE Tahiti has set the standard
+            <motion.div className="flex items-center flex-col py-20 max-lg:py-10" initial={{ opacity: 0, x:50 }} whileInView={{ opacity: 1, x:0 }} transition={{ duration: 1, delay: .25}} viewport={{ amount: .4, once: true}}>
+                <h2 className="heading text-6xl grey w-8/12 max-sm:text-2xl sm:max-lg:text-4xl">OC1 Mana - <span className="orange">Raising The Standard</span> </h2>
+                <div className="flex py-10 justify-around max-lg:flex-col max-lg:items-center max-lg:py-4">
+                    <img src="./mana_1.JPG" alt="Mana OC1" className="w-5/12 max-lg:w-8/12 max-lg:mb-6" />
+                    <div className="w-4/12 max-lg:w-[90%] flex items-center">
+                        <div className="max-md:flex max-md:flex-col max-md:items-center">
+                            <p className="text text-2xl text-left mb-10 max-lg:mb-8 max-md:mb-4 max-sm:text-base sm:max-md:text-lg max-lg:text-center" id="canoe-text-3">For years, ARE Tahiti has set the standard
                                 for outrigger excellence. Now, we've raised the bar again with our
                                 Mana OC1. Balancing craftsmanship and accessability with performance,
                                 the Mana OC1 is ready to take on whatever awaits.
                             </p>
-                            <Link to={'/mana'} className="orange orange-border rounded-3xl headings text-3xl px-16 py-2 orange-button transition-color duration-200 ease-in-out">See More</Link>
+                            <Link to={'/mana'} className="orange orange-border rounded-3xl headings max-md:text-lg max-md:px-10 text-3xl px-16 py-2 orange-button transition-color duration-200 ease-in-out">See More</Link>
                         </div>
                     </div>
                 </div>
             </motion.div>
-            <motion.div className="py-20 flex flex-col items-center" initial={{opacity: 0, y: 200}} whileInView={{opacity: 1, y: 0}} transition={{duration: .5, delay:.25, type:'spring'}} viewport={{amount: .4}}>
-                <h1 className="grey text-6xl heading mb-10">Next Steps</h1>
-                <p className="text text-2xl w-8/12 mb-10">Now that you've chosen the perfect canoe,
+            <motion.div className="py-20 flex flex-col items-center max-lg:py-10" initial={{opacity: 0, y: 200}} whileInView={{opacity: 1, y: 0}} transition={{duration: .5, delay:.25, type:'spring'}} viewport={{amount: .4}}>
+                <h1 className="grey text-6xl heading mb-10 max-sm:text-2xl sm:max-lg:text-4xl max-md:mb-4">Next Steps</h1>
+                <p className="text text-2xl w-8/12 mb-10 max-lg:mb-8 max-md:mb-4 max-sm:text-base sm:max-md:text-lg">Now that you've chosen the perfect canoe,
                     it's time to order it! Click the button below for more information
                     on our ordering process.
                 </p>
-                <Link to={'/order'} className="orange-background orange-bg-button orange-border text-white rounded-3xl headings text-3xl px-16 py-2 transition-color duration-200 ease-in-out">See More</Link>
+                <Link to={'/order'} className="orange-background orange-bg-button orange-border text-white rounded-3xl headings max-md:text-lg max-md:px-10 text-3xl px-16 py-2 transition-color duration-200 ease-in-out">See More</Link>
             </motion.div>
         </div> 
 
--- ARE/src/components/ContactForm.jsx
+++ ARE/src/components/ContactForm.jsx
@@ -75,11 +75,11 @@ const ContactForm = () => {
     }
 
   return (
-    <div className='flex flex-col items-center py-20'>
-            <p className='heading grey text-6xl mb-14'>Contact A Rep</p>
+    <div className='flex flex-col items-center py-20 max-lg:py-10'>
+            <p className='heading grey text-6xl mb-14 sm:max-md:text-4xl max-sm:text-3xl max-md:mb-8'>Contact A Rep</p>
             <form onSubmit={sendEmail} className='bg-neutral-200 border-2 border-black rounded-2xl w-[80%] py-10 flex flex-col items-center px-[2%]' ref={form}>
                 <div className='w-[90%] flex flex-col items-start mb-4'>
-                    <label htmlFor='region' className='text-2xl mb-3 heading grey'>Select a region:</label>
+                    <label htmlFor='region' className='text-2xl mb-3 heading grey max-sm:text-lg'>Select a region:</label>
                     <select id='region' className='border-b bg-transparent border-black text-xl w-full mb-10 pl-2 py-px focus:outline-none focus:bg-white grey order-select' onChange={(e) => handleSelect(e)} value={region} required name='to_email'>
                         <option>Select One...</option>
                         <option value={'billiamisgoated@gmail.com'}>United States</option>
@@ -90,32 +90,32 @@ const ContactForm = () => {
                         <option value={'billiamisgoated@gmail.com'}>Australia</option>
                     </select>
                 </div>
-                <div className='flex justify-around w-full mb-4'>
-                    <div className='w-[40%] flex flex-col items-start'>
-                        <label htmlFor='fName' className='text-2xl mb-3 heading grey'>Enter First Name:</label>
+                <div className='flex justify-around w-full mb-4 max-md:flex-col '>
+                    <div className='w-[40%] flex flex-col items-start max-md:w-[90%] max-md:pl-[6%]'>
+                        <label htmlFor='fName' className='text-2xl mb-3 heading grey max-sm:text-lg'>Enter First Name:</label>
                         <input onChange={(e) => handleChange(e, 'fName')} id='fName' className='border-b bg-transparent border-black text-xl w-full mb-10 pl-2 py-px focus:outline-none focus:bg-white' placeholder='First Name' required name='from_name'/>
                     </div>
-                    <div className='w-[40%] flex flex-col items-start'>
-                        <label htmlFor='fName' className='text-2xl mb-3 heading grey'>Enter Last Name:</label>
+                    <div className='w-[40%] flex flex-col items-start max-md:w-[90%] max-md:pl-[6%]'>
+                        <label htmlFor='fName' className='text-2xl mb-3 heading grey max-sm:text-lg'>Enter Last Name:</label>
                         <input onChange={(e) => handleChange(e, 'lName')} className='border-b bg-transparent border-black text-xl w-full mb-10 pl-2 py-px focus:outline-none focus:bg-white' placeholder='Last Name' required name='from_name'/>
                     </div>
                 </div>
                 <div className='w-[90%] mb-4'>
                     <div className='w-full flex flex-col items-start'>
-                        <label htmlFor='email' className='text-2xl mb-3 heading grey'>Enter Email:</label>
+                        <label htmlFor='email' className='text-2xl mb-3 heading grey max-sm:text-lg'>Enter Email:</label>
                         <input id='email' className='border-b bg-transparent border-black text-xl mb-2 pl-2 py-px focus:outline-none focus:bg-white w-full' placeholder='Email' onChange={(e) => handleChange(e, 'email')} required name='from_email'/>
                         <p className={!validEmail && email.length > 0 ? 'block text-red-600 text-left text-2xl' : 'hidden'}>*Please Enter Valid Email*</p>
                     </div>
                 </div>
                 <div className='w-[90%] mb-4'>
                     <div className='w-full flex flex-col items-start'>
-                        <label htmlFor='email' className='text-2xl mb-3 heading grey'>Enter Phone:</label>
+                        <label htmlFor='email' className='text-2xl mb-3 heading grey max-sm:text-lg'>Enter Phone:</label>
                         <input id='email' className='border-b bg-transparent border-black text-xl mb-2 pl-2 py-px focus:outline-none focus:bg-white w-full' placeholder='Phone' onChange={(e) => handleChange(e, 'phone')} required name='from_phone'/>
                         <p className={!validPhone && phone.length > 0 ? 'block text-red-600 text-left text-2xl' : 'hidden'}>*Please Enter Valid Phone*</p>
                     </div>
                 </div>
                 <div className='w-[90%] mb-10 flex flex-col items-start'>
-                    <label className='grey heading text-2xl mb-3'>Message:</label>
+                    <label className='grey heading text-2xl mb-3 max-sm:text-lg'>Message:</label>
                     <textarea className='w-full p-2 border rounded-xl bg-transparent border-black text-xl w-full mb-10 pl-2 py-2 focus:outline-none focus:bg-white' rows={10} name='message' onChange={(e) => handleChange(e, 'message')}/>
                 </div>
                 <button className={validEmail && fName.length > 0 && lName.length > 0 && region.length > 0 && message.length > 0 ? 'orange-background orange-bg-button orange-border text-white rounded-3xl headings text-3xl px-16 py-2 transition-color duration-200 ease-in-out' : 'orange-background orange-bg-button orange-border text-white rounded-3xl headings text-3xl px-16 py-2 opacity-50'} disabled={validEmail && validPhone ? false : true}>Submit</button>
