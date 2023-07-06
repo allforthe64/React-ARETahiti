fde779c1b11a60191adc72973f0b6da926e81559
William Rainey <williamhrainey@gmail.com>
2023-07-06 12:57:39 -0700

    project is ready for review

--- ARE/src/components/Home.jsx
+++ ARE/src/components/Home.jsx
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom"
 import { motion } from 'framer-motion'
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import ContactForm from "./ContactForm"
 
 const Home = () => {
+
+    const [width, setWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+
+        window.scrollTo(0, 0)
+
+        const handleWindowResize = () => {
+            setWidth(window.innerWidth);
+          };
+      
+          window.addEventListener('resize', handleWindowResize);
+      
+          return () => {
+            window.removeEventListener('resize', handleWindowResize);
+          };
+    }, [])
 
     const discoverVariant = {
         hidden: {
@@ -21,41 +38,8 @@ const Home = () => {
         }
     }
 
-    const containerVariant = {
-        hidden: {
-            opacity: 1,
-            transition: {
-                duration: 0
-            }
-        },
-        show: {
-            opacity: 1,
-            transition: {
-                delay: 1,
-                when: 'beforChildren',
-                staggerChildren: 1
-            }
-        }
-    }
 
-    const cardVariant = {
-        hidden: {
-            opacity: 0,
-            y: 200,
-        },
-        show: {
-            opacity: 1,
-            y: 0,
-            transition: {
-                type: "spring",
-                duration: 1,
-            }
-        }
-    }
-
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
+    console.log(width)
 
     return (
         <div id="en">
@@ -65,16 +49,16 @@ const Home = () => {
             {/*Discover ARE*/}
             <div className="py-32 max-lg:py-14">
                 <motion.h1 className="orange text-8xl max-sm:text-2xl sm:max-lg:text-4xl heading mb-20 max-lg:mb-10" variants={discoverVariant} initial='hidden' whileInView='show' viewport={{amount: .2, once: true}}>Discover ARE</motion.h1>
-                <motion.div className="flex justify-around max-lg:flex-col max-lg:items-center max-lg:space-y-10" variants={containerVariant} initial='hidden' whileInView='show' viewport={{amount: 'all', once: true}}>
-                    <motion.div className="w-3/12 rounded-md bg-neutral-100 p-4 shadowed max-lg:w-7/12" variants={cardVariant}>
-                        <img src="./mana_8.JPG" alt="outrigger-1" className="h-[85%] w-full" />
+                <motion.div className="flex justify-around max-lg:flex-col max-lg:items-center max-lg:space-y-10" initial={{y: 200, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{delay: 1.25, duration: 1}} viewport={{once: true}}>
+                    <motion.div className="w-3/12 rounded-md bg-neutral-100 p-4 shadowed max-lg:w-7/12" >
+                        <img src="./mana_8.JPG" alt="outrigger-1" className="h-[85%] w-full"/>
                         <button type="submit" className="text orange orange-border mt-4 px-6 rounded-md orange-button transition-color ease-in-out duration-200 max-lg:w-7/12 max-sm:text-xs max-sm:w-full" to={'/canoes'}><Link>Get An Outrigger</Link></button>
                     </motion.div>
-                    <motion.div className="w-3/12 rounded-md bg-neutral-100 p-4 shadowed max-lg:w-7/12" variants={cardVariant}>
+                    <motion.div className="w-3/12 rounded-md bg-neutral-100 p-4 shadowed max-lg:w-7/12" >
                         <img src="./marara_6.JPG" alt="outrigger-2" className="h-[85%] w-full" />
                         <button type="submit" className="text orange orange-border mt-4 px-6 rounded-md orange-button transition-color ease-in-out duration-200 max-lg:w-7/12 max-sm:text-xs max-sm:w-full"><Link to={"https://www.youtube.com/channel/UCX1yW5NSmLIGCmXhaD4A98Q"} target="_blank">See Them In Action</Link></button>
                     </motion.div>
-                    <motion.div className="w-3/12 rounded-md bg-neutral-100 p-4 shadowed max-lg:w-7/12 " variants={cardVariant}>
+                    <motion.div className="w-3/12 rounded-md bg-neutral-100 p-4 shadowed max-lg:w-7/12 " >
                         <img src="./matahina_8.jpg" alt="outrigger-3" className="h-[85%] w-full" /> 
                         <button type="submit" className="text orange orange-border mt-4 px-6 rounded-md orange-button transition-color ease-in-out duration-200 max-lg:w-7/12 max-sm:text-xs max-sm:w-full"><Link to={'/order'}>Ordering A Canoe</Link></button>
                     </motion.div>
@@ -85,9 +69,9 @@ const Home = () => {
 
             <div className="py-32 bg-grad-grey max-lg:py-16">
                 <h1 className="text-8xl max-sm:text-2xl sm:max-lg:text-4xl heading text-white mb-16">ARE Tahiti - Born For The Water</h1>    
-                <div className="flex lg:justify-around px-20 max-lg:px-0 max-lg:items-center max-lg:flex-col">
-                    <img className="w-[30%] max-lg:w-[90%]" src="./marara_7.JPG" alt='Outrigger Paddler' id="intro-pic" />
-                    <div className="w-5/12 max-lg:w-[90%] pt-8">
+                <div className="flex lg:justify-between px-20 max-lg:px-0 max-lg:items-center max-lg:flex-col">
+                    <img className="lg:w-[55%] lg:h-[90%] max-lg:w-[90%] max-lg:mb-8" src="./marara_7.JPG" alt='Outrigger Paddler' id="intro-pic" />
+                    <div className="w-5/12 max-lg:w-[90%]">
                         <p className="text-white text text-lg text-left mb-12 max-md:text-base">A lot more goes into an ARE Tahiti va’a, or outrigger, than meets the eye. 
                             To improve your speed and performance on the water, we take painstaking time and effort to perfect every last detail. 
                             Our quest for perfection steers everything we do, including refining the design, engineering and construction of our canoes. 
--- ARE/src/components/Nav.jsx
+++ ARE/src/components/Nav.jsx
@@ -2,6 +2,11 @@ import { Link, useNavigate } from "react-router-dom"
 import useAuth from "../hooks/useAuth"
 import useLogout from "../hooks/useLogout"
 
+import React, { useEffect, useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import './navbar.scss'
+
 const Nav = () => {
 
     const { auth } = useAuth()
@@ -16,7 +21,7 @@ const Nav = () => {
 
     console.log(auth?.accessToken)
 
-    return (
+    /* return (
         <nav className="text-white flex bg-neutral-900 h-14 pt-4" id="nav">
             <Link className="navbar-brand text-white text text-xl pl-[3%] nav-link transition-color ease-in-out duration-200" to={"/"}>ARE Tahiti</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,10 +38,10 @@ const Nav = () => {
                 <li className="nav-item">
                     <Link to={'/canoes'} className="nav-link text-white text nav-link transition-color ease-in-out duration-200">The Canoes</Link>
                 </li>
-                {/* <li className="nav-item">
+                <li className="nav-item">
                     <Link className="nav-link text-white text nav-link transition-color ease-in-out duration-200" href="{% url 'reps' %}">Our Representatives</Link>
-                </li> */}
-                {/* {auth?.accessToken ? (
+                </li> 
+                {auth?.accessToken ? (
                     <li className="nav-link text-white text nav-link transition-color ease-in-out duration-200 hover:cursor-pointer" onClick={() => handleLogout()}>
                     Logout
                     </li>
@@ -51,10 +56,81 @@ const Nav = () => {
                     <li className="nav-item">
                         <Link to={'/admin'} className="nav-link text-white text nav-link transition-color ease-in-out duration-200">Admin Page</Link>
                     </li>
-                } */}
+                }
             </ul>
         </nav>
-    )
+    ) */
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
+  return (
+    <header className="header">
+      <div className="header__content">
+        <Link to="/" className="header__content__logo">
+          ARE Tahiti
+        </Link>
+        <nav
+          className={`${"header__content__nav"} 
+          ${menuOpen && size.width < 768 ? `${"isMenu"}` : ""} 
+          }`}
+        >
+          <ul>
+            <li onClick={menuToggleHandler}>
+              <Link to="/">Home</Link>
+            </li>
+            <li onClick={menuToggleHandler}>
+              <Link to="/about">About</Link>
+            </li>
+            <li onClick={menuToggleHandler}>
+              <Link to="/canoes">The Canoes</Link>
+            </li>
+            <li onClick={menuToggleHandler}>
+              <Link to="/order">Ordering A Canoe</Link>
+            </li>
+
+            {/* <Link to="/register">
+              <button className="btn">Register</button>
+            </Link>
+            <Link to="/login">
+              <button className="btn btn__login">Login</button>
+            </Link> */}
+          </ul>
+        </nav>
+        <div className="header__content__toggle">
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          ) : (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Nav
