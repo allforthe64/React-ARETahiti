a0416b270eccbc56c2ab494a9f8f3a39adf754b3
allforthe64 <williamhrainey@gmail.com>
2025-04-26 12:29:23 -0700

    resend contact form

--- ARE/src/components/ContactForm.jsx
+++ ARE/src/components/ContactForm.jsx
@@ -17,6 +17,7 @@ const ContactForm = () => {
     const [message, setMessage] = useState('')
     const [validEmail, setValidEmail] = useState()
     const [validPhone, setValidPhone] = useState()
+    const [club, setClub] = useState('')
 
     const navigate = useNavigate()
 
@@ -49,23 +50,29 @@ const ContactForm = () => {
     const sendEmail = async (e) => {
         e.preventDefault()
 
-        console.log(import.meta.env.VITE_REACT_EMAILJS_PUBLIC_KEY)
-
-        emailjs.sendForm(import.meta.env.VITE_REACT_EMAILJS_SERVICE_KEY, import.meta.env.VITE_REACT_EMAILJS_TEMPLATE_ID, form.current, import.meta.env.VITE_REACT_EMAILJS_PUBLIC_KEY)
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-
-        
-        form.current.reset()
+        //send the email
+        const response = await fetch('https://aitogearserver.vercel.app/api/newContact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                fName: fName,
+                lName: lName,
+                email: email,
+                phone: phone,
+                club: club,
+                message: message
+            })
+        })
+      
 
         setFName('')
         setLName('')
         setEmail('')
         setPhone('')
-        setRegion('')
+        setClub('')
+        setMessage('')
         setValidEmail(null)
         setValidPhone(null)
         
@@ -78,16 +85,7 @@ const ContactForm = () => {
     <div className='flex flex-col items-center py-20 max-lg:py-10'>
             <p className='heading grey text-6xl mb-14 sm:max-md:text-4xl max-sm:text-3xl max-md:mb-8'>Contact A Rep</p>
             <form onSubmit={sendEmail} className='bg-neutral-200 border-2 border-black rounded-2xl w-[80%] py-10 flex flex-col items-center px-[2%]' ref={form}>
-                <div className='w-[90%] flex flex-col items-start mb-4'>
-                    <label htmlFor='region' className='text-2xl mb-3 heading grey max-sm:text-lg'>Select a region:</label>
-                    <select id='region' className='border-b bg-transparent border-black text-xl w-full mb-10 pl-2 py-px focus:outline-none focus:bg-white grey order-select' onChange={(e) => handleSelect(e)} value={region} required name='to_email'>
-                        <option>Select One...</option>
-                        <option value={'sales@aretahiti.com'}>West Coast</option>
-                        <option value={'sales@aretahiti.com'}>East Coast</option>
-                        <option value={'sales@aretahiti.com'}>Canadian Interior</option>
-                    </select>
-                </div>
-                <div className='flex justify-around w-full mb-4 max-md:flex-col '>
+                <div className='flex justify-around w-full max-md:flex-col '>
                     <div className='w-[40%] flex flex-col items-start max-md:w-[90%] max-md:pl-[6%]'>
                         <label htmlFor='fName' className='text-2xl mb-3 heading grey max-sm:text-lg'>Enter First Name:</label>
                         <input onChange={(e) => handleChange(e, 'fName')} id='fName' className='border-b bg-transparent border-black text-xl w-full mb-10 pl-2 py-px focus:outline-none focus:bg-white' placeholder='First Name' required name='from_name'/>
@@ -111,11 +109,15 @@ const ContactForm = () => {
                         <p className={!validPhone && phone.length > 0 ? 'block text-red-600 text-left text-2xl' : 'hidden'}>*Please Enter Valid Phone*</p>
                     </div>
                 </div>
+                <div className='w-[90%] flex flex-col items-start mb-4'>
+                    <label htmlFor='club' className='text-2xl mb-3 heading grey max-sm:text-lg'>Club affiliation (optional):</label>
+                    <input id='club' value={club} className='border-b bg-transparent border-black text-xl mb-2 pl-2 py-px focus:outline-none focus:bg-white w-full' placeholder='Club affiliation' onChange={(e) => setClub(e.target.value)} required/>
+                </div>
                 <div className='w-[90%] mb-10 flex flex-col items-start'>
                     <label className='grey heading text-2xl mb-3 max-sm:text-lg'>Message:</label>
                     <textarea className='w-full p-2 border rounded-xl bg-transparent border-black text-xl w-full mb-10 pl-2 py-2 focus:outline-none focus:bg-white' rows={10} name='message' onChange={(e) => handleChange(e, 'message')}/>
                 </div>
-                <button className={validEmail && fName.length > 0 && lName.length > 0 && region.length > 0 && message.length > 0 ? 'orange-background orange-bg-button orange-border text-white rounded-3xl max-md:text-lg max-md:px-10 headings text-3xl px-16 py-2 transition-color duration-200 ease-in-out' : 'orange-background orange-bg-button orange-border text-white rounded-3xl max-md:text-lg max-md:px-10 headings text-3xl px-16 py-2 opacity-50'} disabled={validEmail && validPhone ? false : true}>Submit</button>
+                <button className={validEmail && fName.length > 0 && lName.length > 0 && message.length > 0 ? 'orange-background orange-bg-button orange-border text-white rounded-xl max-md:text-lg max-md:px-10 headings text-3xl px-16 py-2 transition-color duration-200 ease-in-out' : 'bg-[#FF3C00] text-white rounded-xl max-md:text-lg max-md:px-10 heading text-3xl px-16 py-2 opacity-50'} disabled={validEmail && validPhone ? false : true}>Submit</button>
             </form>
         </div>
   )
